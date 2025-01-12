2954107bfdd914be627e90eb1d4e5601dd9e2495
Marina <you@example.com>
2025-01-12 21:06:12 +0500

    UI for sing up completed

--- user/src/pages/SignUp.jsx
+++ user/src/pages/SignUp.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>
-      Sign up
+    <div className='mx-auto max-w-lg'>
+      <div className='text-green-600 font-bold text-center text-2xl my-10 uppercase'>Sign up</div>
+      <form className='flex flex-col gap-5'>
+        <input type="text" placeholder='Username' id='Username' className='border p-2'/>
+        <input type="email" placeholder='Email' id='Email' className='border p-2'/>
+        <input type="password" placeholder='Password' id='Password' className='border p-2'/>
+        <button className='p-2 bg-green-500 text-white hover:underline'>Sign Up!</button>
+      </form>
+      <div className='mt-7 text-center'>
+        <Link to='/sign-in'>
+            <p className='text-green-600 hover:underline hidden sm:inline'>Already have an account?</p>
+        </Link>
+      </div>
     </div>
   )
 }
