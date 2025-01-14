72afe65ab535545a246d62c2e19886749e0f0e1a
Marina <you@example.com>
2025-01-14 21:46:57 +0500

    sign in features added

--- user/src/pages/SignIn.jsx
+++ user/src/pages/SignIn.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const res = await fetch('/api/auth/signin', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+  }
+
+  console.log(formData);
+
   return (
     <div className='mx-auto max-w-lg'>
-      <div className='text-green-600 font-bold text-center text-2xl my-10 uppercase'>Sign in</div>
-      <form className='flex flex-col gap-5'>
-        <input type="email" placeholder='Email' id='Email' className='border p-2'/>
-        <input type="password" placeholder='Password' id='Password' className='border p-2'/>
+      <div className='text-green-600 font-bold text-center text-2xl my-10 uppercase'>Sign In</div>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+        <input type="email" placeholder='Email' id='email' className='border p-2' onChange={handleChange} required/>
+        <input type="password" placeholder='Password' id='password' className='border p-2' onChange={handleChange} required/>
         <button className='p-2 bg-green-500 text-white hover:underline'>Sign In!</button>
       </form>
       <div className='mt-7 text-center'>
@@ -17,4 +43,5 @@ export default function SignIn() {
       </div>
     </div>
   )
-};
+}
+
