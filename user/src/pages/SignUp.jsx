ee1444a2036592e4e2cf1ca1b0512aeb43432c94
Marina <you@example.com>
2025-02-07 21:50:38 +0500

    updated signup UI

--- user/src/pages/SignUp.jsx
+++ user/src/pages/SignUp.jsx
@@ -47,12 +47,12 @@ export default function SignUp() {
 
   return (
     <div className='mx-auto max-w-lg'>
-      <div className='text-green-600 font-bold text-center text-2xl my-10 uppercase'>Sign up</div>
+      <div className='text-green-600 font-bold text-center text-2xl my-10'>Welcome to FurryFriends!</div>
       <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
         <input type="text" placeholder='Username' id='username' className='border p-2' onChange={handleChange} required/>
         <input type="email" placeholder='Email' id='email' className='border p-2' onChange={handleChange} required/>
         <input type="password" placeholder='Password' id='password' className='border p-2' onChange={handleChange} required/>
-        <button disabled={loading} className='p-2 bg-green-500 text-white hover:underline'>{loading? 'Wait, loading': 'Sign Up'}</button>
+        <button disabled={loading} className='p-2 bg-rose-400 text-white hover:underline'>{loading? 'Wait, loading': 'Sign Up'}</button>
       </form>
       <div className='mt-7 text-center'>
         <Link to='/sign-in'>
@@ -63,4 +63,6 @@ export default function SignUp() {
     </div>
   )
 }
+
+
 
