a23946fff037b6a0909b766c8d21fd0f8897b7be
Marina <you@example.com>
2025-02-07 21:41:09 +0500

    updated header UI

--- user/src/components/Header.jsx
+++ user/src/components/Header.jsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-yellow-100'>
+    <header className='bg-rose-400'>
         <div className='flex justify-between items-center p-5 max-w-6xl mx-auto'>
             <Link to='/'>
-                <h1 className='font-bold text-sm sm:text-xl'>
-                     <a className='text-green-600'>FurryFriends</a>
+                <h1 className='text-rose-50 font-bold text-sm sm:text-xl'>
+                     <a>FurryFriends</a>
                 </h1>
             </Link> 
             
-          <ul className='text-green-600 flex gap-6'>
+          <ul className='text-rose-50 flex gap-6'>
             <Link to='/'>
                  <li className='hover:underline hidden sm:inline'>Home</li>
             </Link>
@@ -23,9 +23,9 @@ export default function Header() {
             </Link>
           </ul>
 
-          <form className='bg-yellow-50 flex items-center p-3'>
-           <input type='text' placeholder='Search ' className='text-green-600 bg-transparent focus:outline-none w-24 sm:w-64'></input>
-           <FaSearch className='text-green-600'></FaSearch>
+          <form className='bg-rose-50 flex items-center p-3'>
+           <input type='text' placeholder='Search ' className='text-rose-400 bg-transparent focus:outline-none w-24 sm:w-64'></input>
+           <FaSearch className='text-rose-400'></FaSearch>
           </form>
         </div>
     </header>
