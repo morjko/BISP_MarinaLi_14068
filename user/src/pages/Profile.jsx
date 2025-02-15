c78528815166c481c55928088a4107c8304deefa
Marina <you@example.com>
2025-02-15 23:05:28 +0500

    UI for profile page completed

--- user/src/pages/Profile.jsx
+++ user/src/pages/Profile.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>
-      Profile
+    <div className='max-w-lg mx-auto'>
+      <div className='text-green-600 font-bold text-center text-2xl my-10'>My Profile</div>
+      <form className='flex flex-col gap-5'>
+        <input type="text" placeholder='username' id='username' className='border p-2' />
+        <input type="email" placeholder='email' id='email' className='border p-2' />
+        <button className='p-2 bg-green-600 text-white hover:underline'>Edit personal information</button>
+      </form>
+
+      <div className='flex justify-between mt-10'>
+        <button className='bg-transparent hover:text-red-600 hover:underline'>Sign out</button>
+        <button className='bg-transparent hover:text-red-600 hover:underline'>Delete account</button>
+      </div>
     </div>
   )
 }
