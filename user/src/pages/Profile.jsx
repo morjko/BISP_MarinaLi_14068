fc481804174850aef81647ec82c62d4a11bc0d52
Marina <you@example.com>
2025-02-22 20:30:19 +0500

    changed profile page UI

--- user/src/pages/Profile.jsx
+++ user/src/pages/Profile.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {useRef} from 'react';
 
 export default function Profile() {
+  const {currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
   return (
     <div className='max-w-lg mx-auto'>
       <div className='text-green-600 font-bold text-center text-2xl my-10'>My Profile</div>
       <form className='flex flex-col gap-5'>
+        <input hidden type="file" ref={fileRef} accept='image/*' />
+        <img onClick={()=> fileRef.current.click()} className='rounded-full h-20 w-20 cursor-pointer self-center object-cover' src={currentUser.ava} alt="Profile picture" />
         <input type="text" placeholder='username' id='username' className='border p-2' />
         <input type="email" placeholder='email' id='email' className='border p-2' />
         <button className='p-2 bg-green-600 text-white hover:underline'>Edit personal information</button>
